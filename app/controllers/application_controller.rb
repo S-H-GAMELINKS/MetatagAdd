c18deb7da5d9f07e4a10b6c2ed595a8cfe497c49
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-02-15 12:10:28 +0900

    using metatag add

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    before_action :set_meta_tags
+
+    private
+
+        def set_meta_tags
+            @page_title       = 'Foo'
+            @page_description = 'Bar'
+            @page_keywords    = 'Buzz'
+        end
 end
--- app/controllers/web_controller.rb
+++ app/controllers/web_controller.rb
@@ -1,4 +1,11 @@
 class WebController < ApplicationController
   def index
+    @page_title       += 'Foo'
+    @page_description += 'Bar'
+    @page_keywords    += 'Buzz'
+  end
+
+  def hoge
+
   end
 end
